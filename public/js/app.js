0c86e6952f438cf4146b90005eaaabe2304acbdf
unknown <Harry Palmer@DESKTOP-TFMTA3B.simpleclick.local>
2019-09-17 15:24:55 +0100

    Setup App for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,7 @@
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const message0ne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
-
-
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -17,8 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value;
 
 
-
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error);
